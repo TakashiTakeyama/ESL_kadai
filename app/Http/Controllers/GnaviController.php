1b2f15dc4c4666200ffda523ec3a3e0f5d404afb
TakashiTakeyama <takashitakeyama@TakashiTakeyamanoMacBook-Pro.local>
2020-01-27 10:26:07 +0900

    GanviController.php, getAddress.phpを作成

--- app/Http/Controllers/GnaviController.php
+++ app/Http/Controllers/GnaviController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use \GuzzleHttp\Client;
 
 
@@ -10,26 +11,39 @@ class GnaviController extends Controller
 {
     public function index()
     {
-//        $base_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/";
-        $base_url = "https://qiita.com/busyoumono99/items/9b5ffd35dd521bafce47";
+        $base_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/";
+//        $base_url = "https://qiita.com/busyoumono99/items/9b5ffd35dd521bafce47";
 
         $client = new \GuzzleHttp\Client();
 
         $res = $client->request('GET', $base_url, [
-//            'query' => ['keyid' => $_ENV['API_KEY'],
-//                'name' => "魚",
-//            ]
+            'query' => ['keyid' => $_ENV['API_KEY'],
+                'name' => "魚 うなぎ",
+            ]
         ]);
+
+        /**
+         * json形式でresponseを受け取るには？
+         */
+//        $array = $res->json();
+//        print_r($array);
 
         try {
             //assoc: true の場合返り値は連想配列になる。
-//            $json = json_decode($res->getBody(), true);
-//            return view('gnavi.index', $json);
-            var_dump($res) . "\n";
-//            var_dump($json) . "\n";
+            //getBody(): コンテンツを取得する。html要素を取得した。
+            $json = file_get_contents($res);
+            $json = json_decode($res, true);
+//            \Log::info($json);
+            var_dump($json);
+            print_r($json);
+//            var_dump($res);
+//            return $res->json();
+//            $json = (string) $res->getBody();
         } catch (\Exception $e) {
-           return view('welcome');
+            return view('welcome');
 //            $e->getMessage();
+        } finally {
+            return "取得しました。";
         }
     }
 }
---
+++ sample.php
@@ -0,0 +1,60 @@
+<?php
+
+$array = [
+    "name" => "takashi",
+    "gender" => "男",
+    "age" => 35,
+    "email" => "take@gmail.com",
+    "address" => [
+        "pref" => "tokyo",
+        "city" => "suginami",
+        "house_number" => 5-16-6,
+    ]
+];
+
+print_r($array);
+
+echo json_encode($array) . "\n";
+
+class ObjectToArray {
+//    public function __construct()
+//    {
+//
+//    }
+}
+$arr = [
+    "name" => "kanbe",
+    "age" => 28,
+];
+
+$obj = new ObjectToArray($arr);
+
+echo gettype($obj);
+echo json_encode($obj) . "\n";
+echo "ObjectにArray----------------" . "\n";
+
+//$url = "https://qiita.com/fantm21/items/603cbabf2e78cb08133ehttps://qiita.com/fantm21/items/603cbabf2e78cb08133e";
+$url = "https://api.zipaddress.net/?zipcode=4530809";
+echo gettype($url) . "\n"; //戻り値はstring
+
+try {
+    $json = file_get_contents($url);
+//    $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+    echo gettype($json) . "\n"; //戻り値はstring
+    echo "jsonの型";
+    $obj = json_decode($json);
+    echo gettype($obj) . "\n"; //戻り値はobject
+    var_dump($obj);
+    /**
+     * jsonの戻り値はobject(stdClass)になると思う。
+     * json_decode: でobject(stdClass)に変換している。
+     */
+    print_r($obj);
+//    \Log::info($arr);
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+echo gettype($json);
+
