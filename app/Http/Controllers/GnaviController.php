a57b28771067bfb112af7eee15f0bbe37e932805
TakashiTakeyama <takashitakeyama@TakashiTakeyamanoMacBook-Pro.local>
2020-01-27 14:54:03 +0900

    GanviController.php, getAddress.phpを修正

--- app/Http/Controllers/GnaviController.php
+++ app/Http/Controllers/GnaviController.php
@@ -12,7 +12,6 @@ class GnaviController extends Controller
     public function index()
     {
         $base_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/";
-//        $base_url = "https://qiita.com/busyoumono99/items/9b5ffd35dd521bafce47";
 
         $client = new \GuzzleHttp\Client();
 
@@ -24,26 +23,23 @@ class GnaviController extends Controller
 
         /**
          * json形式でresponseを受け取るには？
+         * $array = $res->json();
+         * print_r($array);
          */
-//        $array = $res->json();
-//        print_r($array);
 
         try {
             //assoc: true の場合返り値は連想配列になる。
-            //getBody(): コンテンツを取得する。html要素を取得した。
-            $json = file_get_contents($res);
-            $json = json_decode($res, true);
-//            \Log::info($json);
-            var_dump($json);
-            print_r($json);
-//            var_dump($res);
-//            return $res->json();
-//            $json = (string) $res->getBody();
+            //getBody(): コンテンツを取得する。
+            // (string) キャストしている。
+            $json = (string) $res->getBody();
+            \Log::info($json);
+            return $json;
         } catch (\Exception $e) {
+            \Log::info($e);
             return view('welcome');
-//            $e->getMessage();
-        } finally {
-            return "取得しました。";
+            //finallyの処理が優先されてしまう。
+//         } finally {
+//             return "取得しました。";
         }
     }
 }
